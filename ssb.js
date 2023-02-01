8048d5e1f4cd46d89b84530f6fb37ef210c4284c
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-02-02 00:11:57 +0300

    продукты по фильтрам с пагинацией

--- ssb.js
+++ ssb.js
@@ -2,12 +2,17 @@ const server = require('express')()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const getSelects = require('./utils/getSelects')
+const getProducts = require('./utils/getProducts')
 
 
 server.use(cors())
 server.use(bodyParser.json())
+
 server.post('/selects', async (req, res) => {
   res.status(200).send(await getSelects(req.body))
+})
+server.post('/products', async (req, res) => {
+  res.status(200).send(await getProducts(req.body))
 })
 
 
