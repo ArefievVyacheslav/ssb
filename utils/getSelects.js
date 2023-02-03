7e704c26e538ca20a93087220fbbf247b0c54361
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-02-04 02:40:23 +0300

    сделать роут на получение селекта размеров

--- utils/getSelects.js
+++ utils/getSelects.js
@@ -16,10 +16,20 @@ module.exports = async function getSelects(filtersObj) {
       subcategory_t: productObj.subcategory_t,
     })), 'subcategory')
 
-    const brand = [ ...new Set(products.map(productObj => productObj.brand), 'brand') ]
+    const brand = [ ...new Set(products.map(productObj => productObj.brand)) ]
+
+    const weights = { 'XXXS':-1, 'XXS':0, 'XXS/XS':1, 'XS':2, 'XS/S':3, 'S':4, 'S/M':5, 'M':6, 'M/L':7, 'L':8, 'XL':9, 'XXL':10, 'XXXL':11, 'XXXXL':12 }
+    let size = [ ...new Set(products.map(productObj => productObj.sizes).flat(Infinity)) ]
+    size = size.sort((a, b) => {
+      if (!isNaN(a) && !isNaN(b)) return a - b
+    })
+    size = [ ...size.filter(el => isNaN(el)).sort((a, b) => {
+      if (isNaN(a) && isNaN(b)) return weights[a] - weights[b]
+    }), ...size.filter(el => !isNaN(el)).sort( (a, b) => a.localeCompare(b, undefined, { numeric:true }) ) ]
+
 
     return {
-      subcat, brand
+      subcat, brand, size
     }
   } catch (e) {
     console.log(e);
