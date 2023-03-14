4cb1c5b36074a3960ade48a2fcd599b0efce2834
Kriso <work.kirill.01@gmail.com>
2023-03-14 11:39:29 +0600

    fix излишнего кеша, разделил по телу запроса

--- ssb.js
+++ ssb.js
@@ -1,41 +1,42 @@
-const server = require('express')()
-const cors = require('cors')
+const server = require('express')();
+const cors = require('cors');
 const cache = require('express-redis-cache')({
-  host: 'localhost', port: 6379
+  host: 'localhost', port: 6379,
 });
 
-const bodyParser = require('body-parser')
-const getSelects = require('./services/getSelects')
-const getProducts = require('./services/getProducts')
-const setServiceData = require('./services/setServiceData')
+const bodyParser = require('body-parser');
+const getSelects = require('./services/getSelects');
+const getProducts = require('./services/getProducts');
+const setServiceData = require('./services/setServiceData');
 const SelectsDto = require('./services/dto/selectsDto');
 const PaginationDto = require('./services/dto/paginationDto');
 const LoginDto = require('./services/dto/loginDto');
 const login = require('./services/auth/authService');
 const register = require('./services/auth/registerService');
 
-
-server.use(cors())
-server.use(bodyParser.json())
+server.use(cors());
+server.use(bodyParser.json());
 
 server.post('/login', async (req, res) => {
-  res.status(200).send(await login(new LoginDto(req.body)))
-})
+  res.status(200).send(await login(new LoginDto(req.body)));
+});
 
 server.post('/register', async (req, res) => {
-  res.status(200).send(await register(new LoginDto(req.body)))
-})
+  res.status(200).send(await register(new LoginDto(req.body)));
+});
 
-server.post('/selects', cache.route({ expire: 60 * 60 * 24 }), async (req, res) => {
-  res.status(200).send(await getSelects(new SelectsDto(req.body.findObj, req.body.collection), new PaginationDto(req.body.pagination)))
-})
+server.post('/selects', function (req, res, next) {
+  res.express_redis_cache_name = 'select:' + JSON.stringify(req.body);
+  next();
+}, cache.route({expire: 60 * 60 * 24}), async (req, res) => {
+  res.status(200).send(await getSelects(new SelectsDto(req.body.findObj, req.body.collection), new PaginationDto(req.body.pagination)));
+});
 server.post('/products', async (req, res) => {
-  res.status(200).send(await getProducts(req.body))
-})
+  res.status(200).send(await getProducts(req.body));
+});
 
 server.put('/service', async (req, res) => {
-  res.status(200).send(await setServiceData(req.body))
-})
+  res.status(200).send(await setServiceData(req.body));
+});
 
-
-server.listen(3004, console.log('ON 3004'))
+server.listen(3004, console.log('ON 3004'));
