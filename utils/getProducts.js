01265540c473cbefd5b97daf91c8fd64d9a346dc
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-04-28 20:53:51 +0300

    fix

--- utils/getProducts.js
+++ utils/getProducts.js
@@ -17,7 +17,7 @@ module.exports = async function getSelects(filtersObj) {
 
     const productsTotal = products.map(productObj => ({
       id: productObj.id,
-      category: productObj.category,
+      categoryT: productObj.categoryT,
       brand: productObj.brand,
       name: productObj.name,
       images: productObj.images[0],
