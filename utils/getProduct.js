d3c847db98e7955b5c38ba5023e698de3a935c2a
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-04-28 21:24:53 +0300

    fix

--- utils/getProduct.js
+++ utils/getProduct.js
@@ -6,7 +6,7 @@ module.exports = async function getProduct (param) {
   try {
     await client.connect()
     const db = await client.db('ss')
-    return await db.collection(param.category).findOne(param.id)
+    return await db.collection(param.category).findOne({ id: param.id })
   } catch (e) {
     console.log(e);
   }
