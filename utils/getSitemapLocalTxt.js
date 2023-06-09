656a6883e833e24d60550f92852157dbb826e416
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-09 13:50:33 +0300

    fix

---
+++ utils/getSitemapLocalTxt.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb')
+const client = new MongoClient('mongodb://localhost:27017')
+const { writeFileSync } = require('fs')
+const transliteration = require('../utils/transliteration')
+
+async function getSitemap () {
+  try {
+    await client.connect()
+    const db = await client.db('ss')
+    const collections = [ 'clothes', 'shoes', 'accessories' ]
+    const sitemap = []
+    for (let collection of collections) {
+      const products = await db.collection(collection).find({}).toArray()
+      products.forEach(product => {
+        sitemap.push(`/product/${transliteration(product.name.toLowerCase())}-${collection.toLowerCase()}-${product.id}`)
+      })
+    }
+    await writeFileSync('../ssf/static/sitemap.txt', sitemap.reduce((acc, link) => acc += 'https://sales-search.ru' + link + '\n', ''), 'utf-8', (err) => {})
+    process.exit()
+  } catch (e) {
+    console.log(e)
+  }
+}
