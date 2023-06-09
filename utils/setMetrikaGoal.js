47d2cca396bca89500fe81e604499166d13a162c
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-09 14:19:12 +0300

    fix

--- utils/setMetrikaGoal.js
+++ utils/setMetrikaGoal.js
@@ -11,10 +11,10 @@ module.exports = async function setMetrikaGoal (goal) {
     // Формирование данных для запроса
     const headers = { Authorization: 'OAuth ' + accessToken };
     const payload = {
-      goalId,
-      counterId,
+      goal_id: goalId,
+      counter_id: counterId,
       type: 'goal',
-      orderId: goal.orderId,
+      order_id: goal.orderId,
       price: goal.orderAmount,
       currency: goal.orderCurrency,
       income: goal.income
@@ -38,3 +38,19 @@ module.exports = async function setMetrikaGoal (goal) {
     console.log(e)
   }
 }
+
+
+// const metrika = new YandexMetrika({
+//   counterId: '89951108',
+//   token: '703d9270549a4abe92400392ec46f233'
+// });
+//
+// const conversionData = {
+//   order_id: '12345', // Идентификатор заказа
+//   goal_id: 'Ваш_ID_цели', // Идентификатор цели
+//   datetime: new Date(), // Дата и время конверсии
+//   revenue: 100.50, // Сумма заказа
+//   currency: 'RUB', // Валюта заказа
+//   income: 50.25 // Дополнительная информация о доходе
+// };
+// }
