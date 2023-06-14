91342c07eb5c1b1a33387e33d5e49b3f283ceec9
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-14 22:28:00 +0300

    fix

--- ssb.js
+++ ssb.js
@@ -12,32 +12,31 @@ const setMetrikaGoal = require('./utils/setMetrikaGoal');
 const setLike = require('./utils/setLike');
 const setServiceData = require('./utils/setServiceData');
 
+const redisClient = new Redis({
+  host: 'localhost',
+  port: 6379
+});
+const asyncGet = promisify(redisClient.get).bind(redisClient);
+const asyncSet = promisify(redisClient.set).bind(redisClient);
+
 const server = express();
-const redisClient = new Redis();
 
 server.use(cors());
 server.use(bodyParser.json());
 
-const asyncGet = promisify(redisClient.get).bind(redisClient);
-const asyncSet = promisify(redisClient.set).bind(redisClient);
-
 // Middleware для кэширования
 const cacheMiddleware = async (req, res, next) => {
   const key = req.baseUrl + req.path + JSON.stringify(req.body) + JSON.stringify(req.query);
-  try {
-    const cachedData = await asyncGet(key);
-    if (cachedData) {
-      res.send(cachedData);
-    } else {
-      res.sendResponse = res.send;
-      res.send = async (body) => {
-        await asyncSet(key, body);
-        res.sendResponse(body);
-      };
-      next();
-    }
-  } catch (error) {
-    console.error('Ошибка при получении данных из кэша', error);
+  const cachedData = await asyncGet(key);
+
+  if (cachedData) {
+    res.send(cachedData);
+  } else {
+    res.sendResponse = res.send;
+    res.send = async (body) => {
+      await asyncSet(key, body);
+      res.sendResponse(body);
+    };
     next();
   }
 };
@@ -54,7 +53,7 @@ server.post('/product', cacheMiddleware, async (req, res) => {
   res.status(200).send(await getProduct(req.body));
 });
 
-server.get('/sitemap', async (req, res) => {
+server.get('/sitemap', cacheMiddleware, async (req, res) => {
   res.status(200).send(await getSitemap());
 });
 
@@ -71,15 +70,10 @@ server.put('/service', async (req, res) => {
 });
 
 server.get('/clearCache', async (req, res) => {
-  try {
-    await redisClient.flushall();
-    res.send('Кэш успешно сброшен');
-  } catch (error) {
-    console.error('Ошибка при сбросе кэша', error);
-    res.status(500).send('Ошибка при сбросе кэша');
-  }
+  await redisClient.flushall();
+  res.send('Кэш успешно сброшен');
 });
 
 server.listen(3004, () => {
-  console.log('Сервер запущен на порту 3004');
+  console.log('ON 3004');
 });
