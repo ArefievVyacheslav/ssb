502cd6665df5db43bee5067266fb1240dcf4ff8d
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-14 12:27:36 +0300

    fix cache

--- ssb.js
+++ ssb.js
@@ -63,8 +63,10 @@ server.put('/service', async (req, res) => {
 // Роут для сброса всего кэша
 server.get('/clearCache', (req, res) => {
   cache.flushAll();
+  res.setHeader('Cache-Control', 'no-store');
+  res.setHeader('Expires', '0');
   res.send('Кэш успешно сброшен');
-});
+})
 
 server.listen(3004, () => {
   console.log('ON 3004');
