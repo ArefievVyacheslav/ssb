24974afe56c5cbe162a9b54cf017c091d8fb778c
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-14 23:18:26 +0300

    fix

--- ssb.js
+++ ssb.js
@@ -1,21 +1,19 @@
-const server = require('express')();
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const getSelects = require('./utils/getSelects');
-const getProducts = require('./utils/getProducts');
-const getProduct = require('./utils/getProduct');
-const getSitemap = require('./utils/getSitemap');
-const setLike = require('./utils/setLike');
-const setServiceData = require('./utils/setServiceData');
 const redis = require('redis');
+
+const app = express();
+const port = 3004;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const client = redis.createClient();
 
 client.on('connect', () => {
   console.log('Подключено к Redis');
 });
-
-server.use(cors());
-server.use(bodyParser.json());
 
 // Middleware для кэширования
 function cacheMiddleware(req, res, next) {
@@ -36,69 +34,46 @@ function cacheMiddleware(req, res, next) {
   });
 }
 
-// Роут для удаления глобального кэша
-server.get('/clear-cache', (req, res) => {
-  client.keys('*', (err, keys) => {
-    if (err) {
-      console.error('Ошибка при получении ключей из кэша:', err);
-      res.status(500).send('Ошибка при очистке кэша');
-    } else {
-      if (keys.length > 0) {
-        const multi = client.multi();
-        keys.forEach((key) => {
-          multi.del(key);
-        });
-        multi.exec((err, replies) => {
-          if (err) {
-            console.error('Ошибка при удалении ключей из кэша:', err);
-            res.status(500).send('Ошибка при очистке кэша');
-          } else {
-            console.log('Глобальный кэш успешно очищен');
-            res.status(200).send('Глобальный кэш очищен');
-          }
-        });
-      } else {
-        console.log('Кэш уже пуст');
-        res.status(200).send('Глобальный кэш пуст');
-      }
-    }
-  });
-});
-
-server.post('/selects', cacheMiddleware, async (req, res) => {
+app.post('/selects', cacheMiddleware, async (req, res) => {
   const cacheKey = req.url;
   const data = await getSelects(req.body);
   client.set(cacheKey, JSON.stringify(data)); // Кэширование навсегда
   res.status(200).send(data);
 });
 
-server.post('/products', cacheMiddleware, async (req, res) => {
+app.post('/products', cacheMiddleware, async (req, res) => {
   const cacheKey = req.url;
   const data = await getProducts(req.body);
   client.set(cacheKey, JSON.stringify(data)); // Кэширование навсегда
   res.status(200).send(data);
 });
 
-server.post('/product', cacheMiddleware, async (req, res) => {
+app.post('/product', cacheMiddleware, async (req, res) => {
   const cacheKey = req.url;
   const data = await getProduct(req.body);
   client.set(cacheKey, JSON.stringify(data)); // Кэширование навсегда
   res.status(200).send(data);
 });
 
-server.get('/sitemap', cacheMiddleware, async (req, res) => {
+app.get('/sitemap', cacheMiddleware, async (req, res) => {
   const cacheKey = req.url;
   const data = await getSitemap();
   client.set(cacheKey, JSON.stringify(data)); // Кэширование навсегда
   res.status(200).send(data);
 });
 
-server.post('/like', async (req, res) => {
-  res.status(200).send(await setLike(req.body));
+app.get('/clear-cache', (req, res) => {
+  client.flushall((err, succeeded) => {
+    if (err) {
+      console.error('Ошибка при очистке кэша:', err);
+      res.status(500).send('Ошибка при очистке кэша');
+    } else {
+      console.log('Глобальный кэш успешно очищен');
+      res.status(200).send('Глобальный кэш очищен');
+    }
+  });
 });
 
-server.put('/service', async (req, res) => {
-  res.status(200).send(await setServiceData(req.body));
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
 });
-
-server.listen(3004, console.log('ON 3004'));
