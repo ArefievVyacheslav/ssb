80a1c96fe90f34a4037de443d8ca7b3179dbe662
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-06-15 15:56:58 +0300

    add memory-cache

--- utils/getSelects.js
+++ utils/getSelects.js
@@ -19,7 +19,7 @@ module.exports = async function getSelects(filtersObj) {
       filtersObj.findObj.price[ '$in' ] = [...Array.from(Array(+endPrice - +startPrice + 1).keys(),x => x + +startPrice)]
     }
 
-    if (!products.length && collection !== filtersObj.collection) products = await db.collection(filtersObj.collection).find(filtersObj.findObj).project({
+    if (!products.length && collection !== filtersObj.collection) products = await db.collection(filtersObj.collection).find({}).project({
       subcategory: 1, subcategory_t: 1, brand: 1,
       price: 1,
       sale: 1,
@@ -34,19 +34,27 @@ module.exports = async function getSelects(filtersObj) {
     collection = filtersObj.collection
 
 
-    const productsFiltered = products
-      .filter(product => filtersObj.findObj.brand.$in.includes(product.brand))
-      .filter(product => filtersObj.findObj.country.$in.includes(product.country))
-      .filter(product => product.delivery.some(country => filtersObj.findObj.delivery.$in.includes(country)))
-      .filter(product => product.gender === filtersObj.findObj.gender)
-      .filter(product => filtersObj.findObj.installment === true ? filtersObj.findObj.installment === product.installment : true)
-      .filter(product => product.price >= filtersObj.findObj.price.$in[0] && product.price <= filtersObj.findObj.price.$in[1])
-      .filter(product => product.sale > filtersObj.findObj.sale.$gt)
-      .filter(product => filtersObj.findObj.season.$in.includes(product.season))
-      .filter(product => filtersObj.findObj.shop.$in.includes(product.shop))
-      .filter(product => product.sizes.some(size => filtersObj.findObj.sizes.$in.includes(size)))
-      .filter(product => filtersObj.findObj.style.$in.includes(product.style))
-      .filter(product => filtersObj.findObj.subcategory.$in.includes(product.subcategory))
+    const productsFiltered = products.find(filtersObj.findObj).toArray()
+
+    // const productsFiltered = products.reduce((acc, product) => {
+    //   if (filtersObj.findObj.brand && filtersObj.findObj.brand.$in.includes(product.brand)) acc.push(product)
+    //   if (filtersObj.findObj.country && filtersObj.findObj.country.$in.includes(product.country)) acc.push(product)
+    //   if (filtersObj.findObj.delivery && product.delivery.some(country => filtersObj.findObj.delivery.$in.includes(country))) acc.push(product)
+    //   if (filtersObj.findObj.gender && product.gender === filtersObj.findObj.gender) acc.push(product)
+    //   if (filtersObj.findObj.installment && filtersObj.findObj.installment === true ? filtersObj.findObj.installment === product.installment : true) acc.push(product)
+    //   if (filtersObj.findObj.price && product.price >= filtersObj.findObj.price.$in[0] && product.price <= filtersObj.findObj.price.$in[1]) acc.push(product)
+    //   if (filtersObj.findObj.sale && product.sale > filtersObj.findObj.sale.$gt) acc.push(product)
+    //   if (filtersObj.findObj.season && filtersObj.findObj.season.$in.includes(product.season)) acc.push(product)
+    //   if (filtersObj.findObj.shop && ffiltersObj.findObj.shop.$in.includes(product.shop)) acc.push(product)
+    //   if (filtersObj.findObj.sizes && product.sizes.some(size => filtersObj.findObj.sizes.$in.includes(size))) acc.push(product)
+    //   if (filtersObj.findObj.style && filtersObj.findObj.style.$in.includes(product.style)) acc.push(product)
+    //   if (filtersObj.findObj.subcategory && filtersObj.findObj.subcategory.$in.includes(product.subcategory)) acc.push(product)
+    //
+    //   return acc
+    // }, [])
+    //
+    //   .filter(product => filtersObj.findObj.style.$in.includes(product.style))
+    //   .filter(product => filtersObj.findObj.subcategory.$in.includes(product.subcategory))
 
 
     console.log((new Date() - startDate) / 1000, 's', 'Поиск товаров')
@@ -62,7 +70,7 @@ module.exports = async function getSelects(filtersObj) {
 
     const result = { subCat,brand:[],brandCountry:[],color:[],country:[],price:[],sale:[],season:[],shop:[],style:[] }
 
-    productsFiltered.forEach(productObj => {
+    products.forEach(productObj => {
       if (!result.brand.includes(productObj.brand.toUpperCase())) result.brand.push(productObj.brand.toUpperCase())
       if (!result.price.includes(productObj.price)) result.price.push(productObj.price)
       if (!result.sale.includes(productObj.sale)) result.sale.push(productObj.sale)
