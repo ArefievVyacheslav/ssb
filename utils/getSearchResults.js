bfe54f06d7ffb7256a3bb8a3bacdfb3d5154fe02
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-08-04 11:16:13 +0300

    update

--- utils/getSearchResults.js
+++ utils/getSearchResults.js
@@ -4,26 +4,23 @@ const client = new MongoClient('mongodb://localhost:27017');
 module.exports = async function getSearchResults(searchTerm) {
   try {
     await client.connect();
-    const db = await client.db('ss');
-    const clothes = await db.collection('clothes').find({ $text: { $search: searchTerm } })
-      .project({ score: { $meta: 'textScore' } })
-      .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
+    const db = await client.db('mydatabase');
+    const collections = ['clothes', 'shoes', 'accessories'];
+    const results = [];
 
-    const shoes = await db.collection('shoes').find({ $text: { $search: searchTerm } })
-      .project({ score: { $meta: 'textScore' } })
-      .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
+    for (const collectionName of collections) {
+      const collection = db.collection(collectionName);
+      const result = await collection.find({ $text: { $search: `"${searchTerm}"` } }).toArray();
+      results.push(...result);
+    }
 
-    const accessories = await db.collection('accessories').find({ $text: { $search: searchTerm } })
-      .project({ score: { $meta: 'textScore' } })
-      .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
-
-    client.close();
-    return [...clothes, ...shoes, ...accessories];
+    console.log(results);
+    return results;
   } catch (error) {
     console.error('Failed to get search results:', error);
     throw error;
+  } finally {
+    client.close();
+    console.log('Disconnected from MongoDB');
   }
-}
+};
