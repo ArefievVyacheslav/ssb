7a13ea8491b3b75782cf8a2c7eb95d1655c4e879
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-08-04 11:27:47 +0300

    update

--- utils/getSearchResults.js
+++ utils/getSearchResults.js
@@ -5,17 +5,17 @@ module.exports = async function getSearchResults(searchTerm) {
   try {
     await client.connect();
     const db = await client.db('ss');
-    const clothes = await db.collection('clothes').find({ $text: { $search: searchTerm } })
+    const clothes = await db.collection('clothes').find({ $text: { $search: "${searchTerm}" } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
       .toArray() || [];
 
-    const shoes = await db.collection('shoes').find({ $text: { $search: searchTerm } })
+    const shoes = await db.collection('shoes').find({ $text: { $search: "${searchTerm}" } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
       .toArray() || [];
 
-    const accessories = await db.collection('accessories').find({ $text: { $search: searchTerm } })
+    const accessories = await db.collection('accessories').find({ $text: { $search: "${searchTerm}" } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
       .toArray() || [];
