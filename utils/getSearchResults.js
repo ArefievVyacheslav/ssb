dbc3d63ed79a7c1d75b7608242ceff267f794fef
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-08-09 10:38:55 +0300

    change search algoritm

--- utils/getSearchResults.js
+++ utils/getSearchResults.js
@@ -8,17 +8,62 @@ module.exports = async function getSearchResults(searchTerm) {
     const clothes = await db.collection('clothes').find({ $text: { $search: searchTerm } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
+      .toArray().map(product => ({
+      id: product.id,
+      brand: product.brand,
+      category_t: product.category_t,
+      category: 'clothes',
+      color: product.color,
+      like: product.like,
+      link: product.link,
+      name: product.name,
+      images: product.images,
+      oldprice: product.oldprice,
+      price: product.price,
+      sale: product.sale,
+      shop: product.shop,
+      sizes: product.sizes
+    })) || [];
 
     const shoes = await db.collection('shoes').find({ $text: { $search: searchTerm } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
+      .toArray().map(product => ({
+      id: product.id,
+      brand: product.brand,
+      category_t: product.category_t,
+      category: 'shoes',
+      color: product.color,
+      like: product.like,
+      link: product.link,
+      name: product.name,
+      images: product.images,
+      oldprice: product.oldprice,
+      price: product.price,
+      sale: product.sale,
+      shop: product.shop,
+      sizes: product.sizes
+    })) || [];
 
     const accessories = await db.collection('accessories').find({ $text: { $search: searchTerm } })
       .project({ score: { $meta: 'textScore' } })
       .sort({ score: { $meta: 'textScore' } })
-      .toArray() || [];
+      .toArray().map(product => ({
+      id: product.id,
+      brand: product.brand,
+      category_t: product.category_t,
+      category: 'accessories',
+      color: product.color,
+      like: product.like,
+      link: product.link,
+      name: product.name,
+      images: product.images,
+      oldprice: product.oldprice,
+      price: product.price,
+      sale: product.sale,
+      shop: product.shop,
+      sizes: product.sizes
+    })) || [];
 
     const allResults = [...clothes, ...shoes, ...accessories];
 
