41ced2085e9880d11759f5b1dea43a5e28e9dff0
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-09-13 13:11:18 +0300

    fix search

--- utils/getSearchResults.js
+++ utils/getSearchResults.js
@@ -1,25 +1,28 @@
 const { MongoClient } = require('mongodb');
 const client = new MongoClient('mongodb://localhost:27017');
+const stringSimilarity = require("string-similarity");
 
 module.exports = async function getSearchResults(searchTerm) {
   try {
     await client.connect();
     const db = await client.db('ss');
 
-    return await db.collection('all')
-      .find({
-        $text: {
-          $search: searchTerm,
-          $caseSensitive: false, // Регистронезависимый поиск
-          $diacriticSensitive: false // Поиск без учёта диакритики
-        }
-      }, {
+    const allResults = (await db.collection('all')
+      .find({}, {
         projection: {
           id: 1, brand: 1, category: 1, color: 1, like: 1, link: 1, name: 1,
-          images: 1, oldprice: 1, price: 1, sale: 1, shop: 1, sizes: 1, subcategory: 1
+          images: 1, oldprice: 1, price: 1, sale: 1, shop: 1, sizes: 1
         }
       })
-      .toArray();
+      .toArray())
+      .map(product => ({ ...product, textSearch: product.gender + ' ' + product.subcategory + ' '
+          + product.brand + ' ' + product.category + ' ' + product.color + ' ' + product.shop + ' ' + 'со скидкой '
+          + product.sale + '% ' + product.sizes.reduce((acc, size) => acc += size + ' размера ', '') }))
+
+    allResults.forEach(productObj => productObj.score = stringSimilarity
+      .compareTwoStrings([...new Set(productObj.textSearch.toLowerCase().split(' '))].join(' '), searchTerm.toLowerCase()))
+    // Сортировка по убыванию релевантности
+    return allResults.sort((a, b) => b.score - a.score).filter(product => product.score > searchTerm.length / 100);
   } catch (error) {
     console.error('Failed to get search results:', error);
     throw error;
@@ -27,3 +30,34 @@ module.exports = async function getSearchResults(searchTerm) {
     await client.close();
   }
 }
+
+// const { MongoClient } = require('mongodb');
+// const client = new MongoClient('mongodb://localhost:27017');
+//
+// module.exports = async function getSearchResults(searchTerm) {
+//   try {
+//     await client.connect();
+//     const db = await client.db('ss');
+//
+//     return await db.collection('all')
+//       .find({
+//         $text: {
+//           $search: searchTerm,
+//           $language: "russian", // Указываем язык поиска
+//           $caseSensitive: false, // Регистронезависимый поиск
+//           $diacriticSensitive: false // Поиск без учёта диакритики
+//         }
+//       }, {
+//         projection: {
+//           id: 1, brand: 1, category: 1, color: 1, like: 1, link: 1, name: 1,
+//           images: 1, oldprice: 1, price: 1, sale: 1, shop: 1, sizes: 1, subcategory: 1
+//         }
+//       })
+//       .toArray();
+//   } catch (error) {
+//     console.error('Failed to get search results:', error);
+//     throw error;
+//   } finally {
+//     await client.close();
+//   }
+// }
